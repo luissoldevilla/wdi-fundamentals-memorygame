d516b189d3ab040bc5d93957d5dc35565313ef5c
Kelly Soto <Kelly@Kellys-MacBook-Pro.local>
2018-05-20 21:58:48 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,33 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+console.log("")
+// var cardOne = "queen";
+// var cardTwo = "king";
+// var cardThree = "queen";
+// var cardFour = "king";
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardTwo);
+
+var cards = ["queen", "king", "queen", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped queen");
+
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	console.log("You have selected two cards");
+} if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again");
+}
+
